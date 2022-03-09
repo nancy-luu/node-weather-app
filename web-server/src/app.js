1791553645caba4d0410b23ab549cbfe5a31b64b
nluuarch@gmail.com <nluuarch@gmail.com>
2022-03-09 12:29:08 -0800

    Built JSON http endpoint

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,6 +2,9 @@ const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
 
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
 const app = express()
 
 // console.log(__dirname)
@@ -73,25 +76,40 @@ app.get('/weather', (req, res) => {
         })
     }
 
-    res.send({ 
-        forecast: '50 degrees',
-        location: 'Philadelphia',
-        addresss: req.query.address
-    })
-})
-
-app.get('/products', (req, res) => {
-    if (!req.query.search ){
-        return res.send({
-            error: 'You must provide a search term',
+    geocode(req.query.address, (error, {longitude, latitude, location} = {}) => {
+        // const {longitude, latitude, location} = data
+        if (error) {
+            return res.send({ error })
+        }
+    
+        // second asynchronous operation that has access to final data
+        // utilize the response back from geocode for lat and lon
+        forecast(longitude, latitude, (error, forecastData) => {
+            if (error) {
+                return console.log(error)
+            }
+    
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
         })
-    }
-
-    console.log(req.query.search)
-    res.send({ 
-        products: []
     })
 })
+
+// app.get('/products', (req, res) => {
+//     if (!req.query.search ){
+//         return res.send({
+//             error: 'You must provide a search term',
+//         })
+//     }
+
+//     console.log(req.query.search)
+//     res.send({ 
+//         products: []
+//     })
+// })
 
 app.get('/help/*', (req, res)=> {
     res.render( 'errorpage', {
